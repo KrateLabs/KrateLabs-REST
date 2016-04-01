e218e8b4b3cd322de4c3afc1442fe3f87d944c6e
Denis <carriere.denis@gmail.com>
2016-04-01 09:33:59 -0400

    Add Cleaning script

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,2 +1,20 @@
-sudo docker rm $(sudo docker ps -a -q)
-sudo docker rmi $(sudo docker images -q)
+#!/bin/sh
+CONTAINERS=$(docker ps -a -q)
+IMAGES=$(docker images -q)
+
+# Remove all Docker Containers
+if [ -n "$CONTAINERS" ]; then
+  docker rm $CONTAINERS
+fi
+
+# Remove all Docker Images
+if [ -n "$IMAGES" ]; then
+  docker rmi $IMAGES
+fi
+
+# Remove PNG & SVG Images created
+rm -f $(pwd)**/*.png
+rm -f $(pwd)**/*.svg
+
+# Remove Python Build
+rm -f kratelabs.egg-info/
