0009f281ab52ff1ad9070cdcadb5244f1721e18e
Denis Carriere <carriere.denis@gmail.com>
2016-06-28 00:51:12 -0400

    Accept encoding

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use(upload.array(), (request, response, next) => {
 // CORS Middleware
 app.use((request, response, next) => {
   response.header('Access-Control-Allow-Origin', '*')
-  response.header('Access-Control-Allow-Headers', 'Content-Type,Authorization,Cache-Control,Accept')
+  response.header('Access-Control-Allow-Headers', 'Content-Type,Authorization,Cache-Control,Accept,Accept-Encoding')
   next()
 })
 
