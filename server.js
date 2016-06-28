59ddb6087fe8301eb4b7653841a5450ce6d9a1ac
Denis Carriere <carriere.denis@gmail.com>
2016-06-27 23:56:06 -0400

    Add CORS

--- server.js
+++ server.js
@@ -34,6 +34,14 @@ app.use(upload.array(), (request, response, next) => {
   next()
 })
 
+// CORS Middleware
+app.use((request, response, next) => {
+  response.header('Access-Control-Allow-Origin', '*')
+  response.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+  response.header('Access-Control-Allow-Headers', 'Content-Type')
+  next()
+})
+
 // Register Routes
 app.use('/', routes.api)
 app.use('/user', routes.user)
