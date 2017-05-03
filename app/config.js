f6dfdcb399557a4000f5a234fbea14bbc10d5a5e
Denis <carriere.denis@gmail.com>
2017-05-02 23:38:32 -0400

    update repo

--- app/config.js
+++ app/config.js
@@ -1,10 +1,22 @@
-import uuid from 'node-uuid'
+const uuid = require('node-uuid')
 
-export const PORT = process.env.PORT || 5000
-export const SECRET = process.env.SECRET || uuid.v4()
-export const MAPBOX_ACCESS_TOKEN = process.env.MAPBOX_ACCESS_TOKEN
-export const SHOPIFY_API_KEY = process.env.SHOPIFY_API_KEY
-export const SHOPIFY_PASSWORD = process.env.SHOPIFY_PASSWORD
+const PORT = process.env.PORT || 5000
+const SECRET = process.env.SECRET || uuid.v4()
+const MAPBOX_ACCESS_TOKEN = process.env.MAPBOX_ACCESS_TOKEN
+const SHOPIFY_API_KEY = process.env.SHOPIFY_API_KEY
+const SHOPIFY_PASSWORD = process.env.SHOPIFY_PASSWORD
+const KRATELABS_USERNAME = process.env.KRATELABS_USERNAME
+const KRATELABS_PASSWORD = process.env.KRATELABS_PASSWORD
 if (!MAPBOX_ACCESS_TOKEN) throw new Error('MAPBOX_ACCESS_TOKEN is required')
 if (!SHOPIFY_API_KEY) throw new Error('SHOPIFY_API_KEY is required')
 if (!SHOPIFY_PASSWORD) throw new Error('SHOPIFY_PASSWORD is required')
+
+module.exports = {
+  PORT,
+  SECRET,
+  MAPBOX_ACCESS_TOKEN,
+  SHOPIFY_API_KEY,
+  SHOPIFY_PASSWORD,
+  KRATELABS_USERNAME,
+  KRATELABS_PASSWORD
+}
